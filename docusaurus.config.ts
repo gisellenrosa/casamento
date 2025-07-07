a3e269983c67c7bd34b4ce61e527a07511ae7272
gisellenrosa <gisellenrosa@gmail.com.br>
2025-07-07 16:45:59 -0300

    adjusts configs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,11 +15,8 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
+  url: 'https://gisellenrosa.github.io',
+  baseUrl: '/casamento/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
